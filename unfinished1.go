60a537f6cdc123417e328db4080334ab0c01df6c
ShiroNekuro <73917427+ShiroNekuro@users.noreply.github.com>
2024-05-26 14:07:28 +0700

    Update unfinished1.go

--- unfinished1.go
+++ unfinished1.go
@@ -21,7 +21,7 @@ func options(array [size]money){
     fmt.Println("==================================")
     fmt.Println("1. Tampilkan Data")
     fmt.Println("2. Masukkan Data")
-    fmt.Println("==================================")
+    fmt.Println("3. Edit Data")
     fmt.Println("==================================")
     fmt.Print("Masukkan Opsi : ")
     fmt.Scan(&options)
@@ -30,26 +30,25 @@ func options(array [size]money){
             viewData(array)
         case 2:
             insertData(array)
+        case 3:
+            editData(array)
     }
 }
 
 func viewData(array [size]money){
     var i int
+    i = 0
     fmt.Println("====== Data ======")
-      for i = 0;i <= size;i++ {
-          id := array[i].id
-          tgl := array[i].day
-          bln := array[i].month
-          thn := array[i].year
-          jenis := array[i].inout
-          jumlah := array[i].amount
-          if tgl == 0{
-              break
-          } else {
-              fmt.Printf("%v | %v/%v/%v : %v %v \n",id ,tgl,bln,thn,jenis,jumlah)
-          }
-  }
-  options(array)
+    for i = 0;i <= size;i++ {
+        id := array[i].id
+        tgl := array[i].day
+        bln := array[i].month
+        thn := array[i].year
+        jenis := array[i].inout
+        jumlah := array[i].amount
+        fmt.Printf("%v | %v/%v/%v : %v %v \n",id ,tgl,bln,thn,jenis,jumlah)
+      }
+      options(array)
 }
 
 func insertData(array [size]money){
@@ -74,4 +73,28 @@ func insertData(array [size]money){
     options(array)
 }
 
-func editData()
+func editData(array [size]money){
+    var x, index, i int
+    i = 0
+    index = -1
+    fmt.Println("====== Edit Data ======")
+    fmt.Print("Masukkan ID Data yang diedit : ")
+    fmt.Scan(&x)
+    for i <= idcount && index == -1{
+        if x == array[i].id {
+            index = i
+        }
+        i++
+    }
+    fmt.Println("Edit Tanggal : ")
+    fmt.Scan(&array[index].day)
+    fmt.Println("Edit Bulan : ")
+    fmt.Scan(&array[index].month)
+    fmt.Println("Edit Tahun : ")
+    fmt.Scan(&array[index].year)
+    fmt.Println("Jenis : ")
+    fmt.Scan(&array[index].inout)
+    fmt.Println("Edit Jumlah : ")
+    fmt.Scan(&array[index].amount)
+    options(array)
+}
